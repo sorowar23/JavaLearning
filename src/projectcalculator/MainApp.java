f6b44d2c6ca5e2692838f6933ba1b0d80f4e06b7
sorowar23 <sorowar23@gmail.com>
2025-01-10 14:13:29 -0500

    Updated code for valid input check

--- src/projectcalculator/MainApp.java
+++ src/projectcalculator/MainApp.java
@@ -8,18 +8,21 @@ public class MainApp {
     public static void main(String[] args) {
 
         final String inputExp = ReadInput.read();
-        String numberArr[] = inputExp.split("[-+*/]");
-        String operationsArr[] = inputExp.split("[0-9]+");
 
-        Queue<String> numbers = new LinkedList<>(Arrays.asList(numberArr));
-        Queue<String> operations = new LinkedList<>(Arrays.asList(operationsArr));
+        boolean isValid = isValidExpression(inputExp);
+        if(isValid) {
+            String numberArr[] = inputExp.split("[-+*/]");
+            String operationsArr[] = inputExp.split("[0-9]+");
 
-        Double res = Double.parseDouble(numbers.poll());
-        String opr = operations.poll();
+            Queue<String> numbers = new LinkedList<>(Arrays.asList(numberArr));
+            Queue<String> operations = new LinkedList<>(Arrays.asList(operationsArr));
 
-        while (!numbers.isEmpty()) {
-            opr = operations.poll();
-            Operate operate;
+            Double res = Double.parseDouble(numbers.poll());
+            String opr = operations.poll();
+
+            while (!numbers.isEmpty()) {
+                opr = operations.poll();
+                Operate operate;
                 switch (opr) {
                     case "+":
                         operate = new Add();
@@ -39,7 +42,27 @@ public class MainApp {
                 }
                 Double num = Double.parseDouble(numbers.poll());
                 res = operate.getResult(res, num);
+            }
+            System.out.println("Result of Expression:: " + res);
+        }else{
+            System.out.println("Invalid Expression!!!");
         }
-        System.out.println("Result of Expression:: " + res);
+    }
+    private static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
+
+    private static boolean isValidExpression(String expression) {
+        expression = expression.replaceAll("\\s+", ""); // Remove whitespace
+
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+
+            if (!Character.isDigit(c) && !isOperator(c)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
